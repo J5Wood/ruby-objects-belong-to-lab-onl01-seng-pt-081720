5f8b0880efcf829a3b2b06b2d0edee47490f8d15
Jeremy Wood <js.wood206@gmail.com>
2020-09-11 02:43:00 +0000

    Automatically backed up by Learn

---
+++ lib/post.rb
@@ -0,0 +1,9 @@
+class Post
+
+  attr_accessor :title, :author
+
+  def author
+    self.author = author.name
+  end
+
+end
---
+++ lib/song.rb
@@ -0,0 +1,9 @@
+class Song
+
+  attr_accessor :title
+
+  def artist
+    self.artist = artist.name
+  end
+
+end
